b63322c7bf5622d933ce30a5c311972ab56ec00e
apopma <adam.popma@gmail.com>
2015-07-16 13:27:22 -0700

    start Track model

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,7 @@
 class Track < ActiveRecord::Base
+  validates :title, :album_id, :track_number, presence: true
+  validates :track_number, uniqueness: { scope: [:album_id, :track_number] }
+
+  belongs_to :album
+  belongs_to :band, through: :album
 end
