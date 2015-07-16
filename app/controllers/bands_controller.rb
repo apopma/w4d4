2c4ff6a16f2ebf004b9bc6f20122196e032d6272
apopma <adam.popma@gmail.com>
2015-07-16 14:54:00 -0700

    create and destroy bands

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -8,4 +8,32 @@ class BandsController < ApplicationController
     @band = Band.find(params[:id])
     render :show
   end
+
+  def new
+    render :new
+  end
+
+  def create
+    @band = Band.new(band_params)
+
+    if @band.save
+      flash[:success] = ["#{@band.name} will make it big someday! Probably."]
+      redirect_to band_url(@band.id)
+    else
+      flash[:errors] = ["Oops. Something went wrong:"]
+      render :new
+    end
+  end
+
+  def destroy
+    @band = Band.find(params[:id])
+    @band.destroy!
+    flash[:success] = ["#{@band.name} has broken up."]
+    redirect_to bands_url
+  end
+
+  private
+  def band_params
+    params.require(:band).permit(:name)
+  end
 end
