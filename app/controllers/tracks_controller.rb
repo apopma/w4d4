a48ac96bdd73cfdb4329039b736fe16b71dc6342
apopma <adam.popma@gmail.com>
2015-07-16 16:39:16 -0700

    add tracks

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -3,4 +3,28 @@ class TracksController < ApplicationController
     @track = Track.find(params[:id])
     render :show
   end
+
+  def new
+    @album = Album.find(params[:album_id])
+    @albums = @album.band.albums
+    render :new
+  end
+
+  def create
+    @track = Track.new(track_params)
+
+    if @track.save
+      flash[:success] = ["#{@track.band.name} has written '#{@track.title}'!"]
+      redirect_to track_url(@track.id)
+    else
+      flash[:errors] = ["Oops, something went wrong:"]
+      flash[:errors].concat @track.errors.full_messages
+      redirect_to new_album_track_url(@track.album_id)
+    end
+  end
+
+  private
+  def track_params
+    params.require(:track).permit(:title, :track_number, :album_id, :lyrics)
+  end
 end
