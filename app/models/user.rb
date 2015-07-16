773612209809589a7cf67629b71683ea1fdb09f6
apopma <adam.popma@gmail.com>
2015-07-16 10:36:29 -0700

    create User model with authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,33 @@
 class User < ActiveRecord::Base
+  validates :email, :password_digest, :session_token,
+            presence: true, uniqueness: true
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.find_by_credentials(creds)
+    usr = User.find_by_email(creds[:email])
+    usr.is_password?(creds[:password]) ? usr : nil
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+  end
 end
